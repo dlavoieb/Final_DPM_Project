4ddab3762d502d096dc79a3ee4fd7af419377df3
David Lavoie-Boutin <d.lavoie.boutin@me.com>
2014-11-03 17:30:25 -0500

    comment to mission planner

--- src/dpm/lejos/project/MissionPlanner.java
+++ src/dpm/lejos/project/MissionPlanner.java
@@ -14,8 +14,24 @@ public class MissionPlanner {
 	public Grabber m_Grabber;
 	public Localizer m_Localizer;
 
-	public MissionPlanner(){
+	public MissionPlanner() {
 
-	}
+    }
+
+    /**
+     * perform the mission
+     *
+     * steps:
+     *
+     * 1- Localise within the playground
+     * 2- Navigate to pickup zone
+     * 3- Pickup blocks
+     * 4- Navigate to drop off area
+     * 5- Drop blocks
+     * 6- repeat from 2 onwards
+     */
+    public void startMission(){}
+
+
 
 }//end MissionPlanner
