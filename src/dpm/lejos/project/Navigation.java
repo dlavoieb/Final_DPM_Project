a11ca47f3f13c04f2d7deca9d2ebaa3611f69fc8
David Lavoie-Boutin <d.lavoie.boutin@me.com>
2014-12-03 17:07:17 -0500

    commented vector

--- src/dpm/lejos/project/Navigation.java
+++ src/dpm/lejos/project/Navigation.java
@@ -117,6 +117,16 @@ public class Navigation {
                         queue.add(node);
                         node.setVisited(true);
                         node.setPrevious(current);
+                        //
+                        // Dear maintainer:
+                        //
+                        // Once you are done trying to 'optimize' this routine,
+                        // and have realized what a terrible mistake that was,
+                        // please increment the following counter as a warning
+                        // to the next guy:
+                        //
+                        // total_hours_wasted_here = 42
+                        //
                     }
                 }
             }
